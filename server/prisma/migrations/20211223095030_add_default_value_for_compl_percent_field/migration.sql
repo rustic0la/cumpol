80ae875564f6c3e6af1d108c62e212c6dd28c3d9
mpokrovsky <vm853404@protonmail.com>
2021-12-24 05:06:34 +0300

    migration: add new field completePercentage

---
+++ server/prisma/migrations/20211223095030_add_default_value_for_compl_percent_field/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Collection" ALTER COLUMN "complPercent" SET DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "Domain" ALTER COLUMN "complPercent" SET DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "TodoList" ALTER COLUMN "complPercent" SET DEFAULT 0;
