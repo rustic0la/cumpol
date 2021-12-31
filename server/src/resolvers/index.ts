28c02a68514eca3fa3cdf07ad69f744b78318145
mpokrovsky <vm853404@protonmail.com>
2021-12-31 04:32:32 +0300

    remove comment

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -1,4 +1,3 @@
-//       if (!user) return new ForbiddenError("Not Authorized");
 import { Resolvers } from '../generated/types';
 import Mutation from './mutations';
 import Query from './queries';
