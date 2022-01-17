ba24ef9bd6cab6733f51904ec7cc7553bedf152f
mpokrovsky <vm853404@protonmail.com>
2022-01-17 03:35:45 +0300

    configuring app routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,56 @@
-import React, { FC, SyntheticEvent, useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { FC } from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
 
-import logo from './assets/logo.svg';
-import Body from './components/Body';
-import { AUTH_TOKEN } from './constants';
-import { Login } from './pages/Login';
-import { Header, Layout, LogoStyled } from './styles';
+import { AuthProvider, RequireAuth } from './AuthContext';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Sidebar from './components/Sidebar';
+import Auth from './pages/Auth';
+import { GridStyled, LayoutStyled } from './styles';
+
+// TODO: themes
 // ffffff-00fff6-1ba5ab-364b60
 // ffbda0-ffa780-ff7b40-ff4f00-ff0000-c00000-800000-400000-200000
 
-const Layout1: FC = () => {
+const App: FC = () => {
   return (
-    <>
-      <Header>
-        <LogoStyled>
-          <img src={logo} alt="logo" width="60px" />
-          <h1>cumpol</h1>
-        </LogoStyled>
-        <div>username</div>
-      </Header>
-      <Body />
-    </>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/auth" element={<Auth />} />
+          <Route
+            path="/"
+            element={
+              <RequireAuth>
+                <MainPage />
+              </RequireAuth>
+            }
+          >
+            <Route index element={<h1>Select Space</h1>} />
+            <Route path=":spaceId" element={<MainContent />} />
+          </Route>
+          <Route path="*" element={<h1>Not found</h1>} />
+        </Route>
+      </Routes>
+    </AuthProvider>
   );
 };
 
-const App: FC = () => {
-  const authToken = localStorage.getItem(AUTH_TOKEN);
-
+const Layout: FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout>{authToken ? <Layout1 /> : <Login />}</Layout>} />
-    </Routes>
+    <LayoutStyled>
+      <Header />
+      <Outlet />
+    </LayoutStyled>
+  );
+};
+
+const MainPage: FC = () => {
+  return (
+    <GridStyled>
+      <Sidebar />
+      <Outlet />
+    </GridStyled>
   );
 };
 
