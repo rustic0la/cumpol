94852015cf85f668a23fbe4d1562e9536707607f
mpokrovsky <vm853404@protonmail.com>
2022-01-27 04:20:03 +0300

    fix app for production

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,11 @@ const startServer = async () => {
   app.use(express.json());
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'client', 'build', 'index.html')));
+    app.use(express.static(path.resolve(__dirname, '../..', 'client', 'build')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../..', 'client', 'build', 'index.html'));
+    });
   }
 
   const httpServer = createServer(app);
